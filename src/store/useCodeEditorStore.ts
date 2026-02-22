521e7b586ad7bf6c66d98f147613860f5113862c
JaimilModi <162970898+JaimilModi@users.noreply.github.com>
2026-02-22 20:47:46 +0530

    piston fucked up

--- src/store/useCodeEditorStore.ts
+++ src/store/useCodeEditorStore.ts
@@ -60,7 +60,81 @@ export const useCodeEditorStore = create<CodeEditorState>((set, get) => {
       });
     },
     runCode: async () => {
-      //TODO
+      const { language, getCode } = get();
+      const code = getCode();
+
+      if (!code) {
+        set({ error: "Enter Some Code" });
+        return;
+      }
+      set({ isRunning: true, error: null, output: "" });
+      try {
+        const runtime = LANGUAGE_CONFIG[language].pistonRuntime;
+        const response = await fetch("https://emkc.org/api/v2/piston/execute", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            language: runtime.language,
+            version: runtime.version,
+            files: [{ content: code }],
+          }),
+        });
+        const data = await response.json();
+        console.log("data from piston:", data);
+
+        //api fati gayu
+        //TODO
+        if (data.message) {
+          set({
+            error: data.message,
+            executionResult: { code, output: "", error: data.message },
+          });
+          return;
+        }
+
+        //compilation sachavo benchooo
+        if (data.compile && data.compile.code !== 0) {
+          const error = data.compile.stderr || data.compile.output;
+          set({
+            error,
+            executionResult: {
+              code,
+              output: "",
+              error,
+            },
+          });
+          return;
+        }
+        if (data.run && data.run.code !== 0) {
+          const error = data.run.stderr || data.run.output;
+          set({
+            error,
+            executionResult: {
+              code,
+              output: "",
+              error,
+            },
+          });
+          return;
+        }
+        //chali jay tyare
+        const output = data.run.output;
+        set({
+          output: output.trim(),
+          error: null,
+          executionResult: { code, output: output.trim(), error: null },
+        });
+      } catch (error) {
+        console.log("Error running code:", error);
+        set({
+          error: "Error running code",
+          executionResult: { code, output: "", error: "Error running code" },
+        });
+      } finally {
+        set({ isRunning: false });
+      }
     },
   };
 });
